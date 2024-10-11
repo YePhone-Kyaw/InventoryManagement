c09c10caf96df27f7adc26260b2dfeb7a3eb8bfa
Ye Phone Kyaw <yephonekyaw920@gmail.com>
2024-10-11 07:45:32 -0600

    yp styling update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,82 @@
+const { createThemes } = require('tw-colors');
+const colors = require('tailwindcss/colors');
+
+const baseColors = [
+  "gray",
+  "red",
+  "yellow",
+  "green",
+  "blue",
+  "indigo",
+  "purple",
+  "pink",
+];
+
+const shadeMapping = {
+  "50": "900",
+  "100": "800",
+  "200": "700",
+  "300": "600",
+  "400": "500",
+  "500": "400",
+  "600": "300",
+  "700": "200",
+  "800": "100",
+  "900": "50",
+};
+
+const generateThemeObject = (colors, mapping, invert = false) => {
+  const theme = {};
+  baseColors.forEach((color) => {
+    theme[color] = {};
+    Object.entries(mapping).forEach(([key, value]) => {
+      const shadeKey = invert ? value : key;
+      theme[color][key] = colors[color][shadeKey];
+    });
+  });
+  return theme;
+};
+
+const lightTheme = generateThemeObject(colors, shadeMapping);
+const darkTheme = generateThemeObject(colors, shadeMapping, true);
+
+const customPurple = '#3F0071'; // Adjust this to your desired purple shade
+
+const themes = {
+  light: {
+    ...lightTheme,
+    white: "#ffffff",
+    background: "#ffffff",
+    foreground: colors.slate["950"],
+  },
+  dark: {
+    ...darkTheme,
+    white: colors.gray["950"],
+    black: colors.gray["50"],
+    background: customPurple,
+    foreground: colors.slate["100"],
+  },
+};
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,jsx,mdx}",
+    "./components/**/*.{js,jsx,mdx}",
+    "./app/**/*.{js,jsx,mdx}",
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'custom-purple': customPurple,
       },
     },
   },
-  plugins: [],
+  plugins: [createThemes(themes)],
 };
