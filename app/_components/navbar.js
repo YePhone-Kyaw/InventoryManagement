2dfd97c98635288a5ce784935fc1b5c6ba694c09
Ye Phone Kyaw <yephonekyaw920@gmail.com>
2024-10-11 07:10:34 -0600

    yp setting page commit

--- app/_components/navbar.js
+++ app/_components/navbar.js
@@ -1,23 +1,38 @@
 "use client";
 
-import { Bell, Moon, Search, Sun } from "lucide-react";
-import { useState } from "react";
+import { Bell, Menu, Moon, Search, Settings, Sun } from "lucide-react";
+import { useAppDispatch, useAppSelector } from "../redux";
+import { setIsDarkMode, setIsSidebarCollapsed } from "@/state/page";
+import Link from "next/link";
 
 export default function NavigationBar() {
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
 
-    const [isDarkMode, setIsDarkMode] = useState(false);
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
 
-    const changeTheme = () => {
-        setIsDarkMode(!isDarkMode);
-        document.documentElement.classList.toggle('dark');
-    };
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+  };
 
+  const toggleDarkMode = () => {
+    dispatch(setIsDarkMode(!isDarkMode));
+    // document.documentElement.classList.toggle("dark");
+  };
 
   return (
     <main className="flex justify-between items-center w-full mb-7">
-      {/* left sidebar */}
       <div className="flex justify-between items-center gap-5">
-      <div className="relative">
+        <button
+          className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
+          onClick={toggleSidebar}
+        >
+          <Menu className="w-4 h-4" />
+        </button>
+
+        <div className="relative">
           <input
             type="search"
             placeholder="Search products"
@@ -29,17 +44,30 @@ export default function NavigationBar() {
           </div>
         </div>
       </div>
-      <div className="flex ">
-        <div>
-            <button className={`toggle-button`} onClick={changeTheme}>
-                {isDarkMode ? (
-            <Sun className="cursor-pointer text-gray-500" size={20} />
-                    
-                ) : (
-                    <Moon className="cursor-pointer text-gray-500" size={20} />
-                )}
+      <div className="flex justify-between items-center gap-5">
+        <div className="hidden md:flex justify-between items-center gap-5">
+          <div>
+            <button  onClick={toggleDarkMode}>
+              {isDarkMode ? (
+                <Sun className="cursor-pointer text-gray-500" size={24} />
+              ) : (
+                <Moon className="cursor-pointer text-gray-500" size={24} />
+              )}
             </button>
+          </div>
+          <div className="relative">
+            <Bell className="cursor-pointer text-gray-500" size={24} />
+          </div>
+          <hr className="w-0 h-7 border border-solid border-1 border-gray-300 mx-3" />
+          <div className="flex items-center gap-3 cursor-pointer">
+              <img src="" alt="User Profile" className="rounded-full h-full object-cover w-50 h-50" />
+              {/* <img src={user.photoURL} alt="User Profile" className="rounded-full h-full object-cover w-50 h-50" />
+              <span>{user.displayName}</span> */}
+          </div>
         </div>
+        <Link href="/setting">
+            <Settings className="cursor-pointer text-gray-500" size={24} />
+        </Link>
       </div>
     </main>
   );
