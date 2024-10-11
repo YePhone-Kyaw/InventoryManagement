c9f8d0ad3a4ba389e17b7e41dc67d482afdbb7cd
Ye Phone Kyaw <yephonekyaw920@gmail.com>
2024-10-10 19:58:19 -0600

    yp third commit (update navbar and sidebar)

--- app/_components/sidebar.js
+++ app/_components/sidebar.js
@@ -1,16 +1,126 @@
-import NavigationBar from "./navbar";
+import { Layout, Menu, Archive, CircleDollarSign, Clipboard, User, SlidersHorizontal } from "lucide-react";
 
+const { default: Link } = require("next/link");
+const { usePathname } = require("next/navigation");
+const { useAppDispatch, useAppSelector } = require("../redux");
+const { setIsSidebarCollapsed } = require("@/state/page");
 
-export default function SideBar({children}) {
+function SidebarLink({ href, icon: Icon, label, isCollapsed }) {
+  const pathName = usePathname();
+  const isActive =
+    pathName === href || (pathName === "/" && href === "/dashboard");
 
-    return (
-        <main className={`light flex bg-gray-50 text-gray-700 w-full min-h-screen`}>
-            Side Bar
-            <div>
-                <NavigationBar />
-                {children}
-            </div>
-        </main>
+  return (
+    <Link href={href}>
+      <div
+        className={`cursor-pointer flex items-center ${
+          isCollapsed ? "justify-center py-4" : "justify-start px-8 py-4"
+        } hover:text-blue-500 hover:bg-blue-100 gap-3 transition-colors ${
+          isActive ? "bg-blue-200 text-white" : ""
+        }`}
+      >
+        <Icon className="w-6 h-6 !text-gray-700" />
 
-    )
-}
+        <span
+          className={`${
+            isCollapsed ? "hidden" : "block"
+          } font-medium text-gray-700`}
+        >
+          {label}
+        </span>
+      </div>
+    </Link>
+  );
+};
+
+export default function SideBar() {
+    const dispatch = useAppDispatch();
+    const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+    );
+
+    const toggleSidebar = () => {
+        dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+    };
+
+    const sidebarClassNames = `fixed flex flex-col ${
+        isSidebarCollapsed ? "w-0 md:w-16" : "w-72 md:w-64"
+      } bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`;
+      return (
+        <div className={sidebarClassNames}>
+          {/* TOP LOGO */}
+          <div
+            className={`flex gap-3 justify-between md:justify-normal items-center pt-8 ${
+              isSidebarCollapsed ? "px-5" : "px-8"
+            }`}
+          >
+            <image
+              src="https://s3-inventorymanagement.s3.us-east-2.amazonaws.com/logo.png"
+              alt="edstock-logo"
+              width={27}
+              height={27}
+              className="rounded w-8"
+            />
+            <h1
+              className={`${
+                isSidebarCollapsed ? "hidden" : "block"
+              } font-extrabold text-2xl`}
+            >
+              EDSTOCK
+            </h1>
+    
+            <button
+              className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
+              onClick={toggleSidebar}
+            >
+              <Menu className="w-4 h-4" />
+            </button>
+          </div>
+    
+          {/* LINKS */}
+          <div className="flex-grow mt-8">
+            <SidebarLink
+              href="/dashboard"
+              icon={Layout}
+              label="Dashboard"
+              isCollapsed={isSidebarCollapsed}
+            />
+            <SidebarLink
+              href="/inventory"
+              icon={Archive}
+              label="Inventory"
+              isCollapsed={isSidebarCollapsed}
+            />
+            <SidebarLink
+              href="/products"
+              icon={Clipboard}
+              label="Products"
+              isCollapsed={isSidebarCollapsed}
+            />
+            <SidebarLink
+              href="/users"
+              icon={User}
+              label="Users"
+              isCollapsed={isSidebarCollapsed}
+            />
+            <SidebarLink
+              href="/settings"
+              icon={SlidersHorizontal}
+              label="Settings"
+              isCollapsed={isSidebarCollapsed}
+            />
+            <SidebarLink
+              href="/expenses"
+              icon={CircleDollarSign}
+              label="Expenses"
+              isCollapsed={isSidebarCollapsed}
+            />
+          </div>
+    
+          {/* FOOTER */}
+          <div className={`${isSidebarCollapsed ? "hidden" : "block"} mb-10`}>
+            <p className="text-center text-xs text-gray-500">&copy; 2024 Edstock</p>
+          </div>
+        </div>
+      );
+};
